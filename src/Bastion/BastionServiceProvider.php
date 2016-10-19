c0feebb845e1ae463f8af2e3eeead53e854a2403
donnysim <donatas.smk@gmail.com>
2016-10-19 23:03:43 +0300

    Add service provider.

--- src/Bastion/BastionServiceProvider.php
+++ src/Bastion/BastionServiceProvider.php
@@ -2,17 +2,61 @@
 
 namespace Ethereal\Bastion;
 
+use Ethereal\Bastion\Store\Store;
+use Ethereal\Cache\GroupFileStore;
+use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class BastionServiceProvider extends ServiceProvider
 {
     /**
      * Register the service provider.
-     *
-     * @return void
      */
     public function register()
     {
-        // TODO: Implement register() method.
+        $this->registerStore();
+        $this->registerBastion();
+    }
+
+    /**
+     * Register bastion at gate.
+     */
+    public function boot()
+    {
+        $this->registerAtGate();
+    }
+
+    /**
+     * Register singleton Store instance.
+     */
+    protected function registerStore()
+    {
+        $this->app->singleton(Store::class, function () {
+            return new Store(new GroupFileStore($this->app['files'], storage_path('cache/bastion')));
+        });
+    }
+
+    /**
+     * Register bastion instance.
+     */
+    protected function registerBastion()
+    {
+        $this->app->singleton(Bastion::class, function () {
+            $bastion = new Bastion(
+                $this->app->make(Gate::class),
+                $this->app->make(Store::class)
+            );
+
+            return $bastion;
+        });
+    }
+
+    /**
+     * Register bastion at gate.
+     */
+    protected function registerAtGate()
+    {
+        $store = $this->app->make(Store::class);
+        $store->registerAt($this->app->make(Gate::class));
     }
 }
