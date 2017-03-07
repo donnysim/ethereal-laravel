d613627b81ebedd4803115414df5fe4fc9a86e83
donnysim <donatas.smk@gmail.com>
2017-03-07 09:58:59 +0200

    Revert extraction, implement ManagesProxy.

--- src/Bastion/Bastion.php
+++ src/Bastion/Bastion.php
@@ -10,7 +10,6 @@ use Ethereal\Bastion\Conductors\GivesAbilities;
 use Ethereal\Bastion\Conductors\ManageProxy;
 use Ethereal\Bastion\Conductors\RemovesAbilities;
 use Ethereal\Bastion\Conductors\RemovesRoles;
-use Ethereal\Bastion\Conductors\Traits\ManagesAuthority;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,8 +21,6 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Bastion
 {
-    use ManagesAuthority;
-
     /**
      * Default rucks type to use.
      *
@@ -72,6 +69,66 @@ class Bastion
     }
 
     /**
+     * Start a chain to assign the given role to authority.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $roles
+     *
+     * @return \Ethereal\Bastion\Conductors\AssignsRoles
+     */
+    public function assign($roles)
+    {
+        return new AssignsRoles($this->getStore(), is_array($roles) ? $roles : func_get_args());
+    }
+
+    /**
+     * Start a chain to remove the given role from authority.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $roles
+     *
+     * @return \Ethereal\Bastion\Conductors\RemovesRoles
+     */
+    public function retract($roles)
+    {
+        return new RemovesRoles($this->getStore(), is_array($roles) ? $roles : func_get_args());
+    }
+
+    /**
+     * Start a chain to give abilities to authorities.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $authorities
+     *
+     * @return \Ethereal\Bastion\Conductors\GivesAbilities
+     */
+    public function allow($authorities)
+    {
+        return new GivesAbilities($this->getStore(), is_array($authorities) ? $authorities : func_get_args(), false);
+    }
+
+    /**
+     * Start a chain to remove abilities from authorities.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $authorities
+     *
+     * @return \Ethereal\Bastion\Conductors\RemovesAbilities
+     */
+    public function disallow($authorities)
+    {
+        return new RemovesAbilities($this->getStore(), is_array($authorities) ? $authorities : func_get_args(), false);
+    }
+
+    /**
+     * Start a chain to forbid abilities to authorities.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $authorities
+     *
+     * @return \Ethereal\Bastion\Conductors\GivesAbilities
+     */
+    public function forbid($authorities)
+    {
+        return new GivesAbilities($this->getStore(), is_array($authorities) ? $authorities : func_get_args(), true);
+    }
+
+    /**
      * Start a chain to manage authority roles or abilities.
      *
      * @param \Illuminate\Database\Eloquent\Model $authority
@@ -81,6 +138,18 @@ class Bastion
     public function manage(Model $authority)
     {
         return new ManageProxy($this->getStore(), $authority);
+    }
+
+    /**
+     * Start a chain to permit forbidden abilities from authorities.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $authorities
+     *
+     * @return \Ethereal\Bastion\Conductors\RemovesAbilities
+     */
+    public function permit($authorities)
+    {
+        return new RemovesAbilities($this->getStore(), is_array($authorities) ? $authorities : func_get_args(), true);
     }
 
     /**
--- src/Bastion/Conductors/ManageProxy.php
+++ src/Bastion/Conductors/ManageProxy.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ManageProxy
 {
-    use Traits\UsesScopes, Traits\ManagesAuthority;
+    use Traits\UsesScopes;
 
     /**
      * Permission store.
@@ -32,5 +32,105 @@ class ManageProxy
     {
         $this->store = $store;
         $this->authority = $authority;
+    }
+
+    /**
+     * Assign the given role to authority.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $roles
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \InvalidArgumentException
+     */
+    public function assign($roles)
+    {
+        (new AssignsRoles($this->store, is_array($roles) ? $roles : func_get_args()))->to($this->authority);
+    }
+
+    /**
+     * Remove the given role from authority.
+     *
+     * @param array|string|\Illuminate\Database\Eloquent\Model $roles
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \InvalidArgumentException
+     */
+    public function retract($roles)
+    {
+        (new RemovesRoles($this->store, is_array($roles) ? $roles : func_get_args()))->from($this->authority);
+    }
+
+    /**
+     * Give abilities to authorities.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|array|string $abilities
+     * @param \Illuminate\Database\Eloquent\Model|array|string|null $modelListOrClass
+     * @param array|string|int|null $ids
+     *
+     * @throws \InvalidArgumentException
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function allow($abilities, $modelListOrClass = null, $ids = null)
+    {
+        (new GivesAbilities($this->store, [$this->authority], false))
+            ->group($this->scopeGroup)
+            ->parent($this->scopeParent)
+            ->to($abilities, $modelListOrClass, $ids);
+    }
+
+    /**
+     * Remove abilities from authorities.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|array|string $abilities
+     * @param \Illuminate\Database\Eloquent\Model|array|string|null $modelListOrClass
+     * @param array|string|int|null $ids
+     *
+     * @throws \InvalidArgumentException
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \UnexpectedValueException
+     */
+    public function disallow($abilities, $modelListOrClass = null, $ids = null)
+    {
+        (new RemovesAbilities($this->store, [$this->authority], false))
+            ->group($this->scopeGroup)
+            ->parent($this->scopeParent)
+            ->to($abilities, $modelListOrClass, $ids);
+    }
+
+    /**
+     * Forbid abilities to authorities.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|array|string $abilities
+     * @param \Illuminate\Database\Eloquent\Model|array|string|null $modelListOrClass
+     * @param array|string|int|null $ids
+     *
+     * @throws \InvalidArgumentException
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function forbid($abilities, $modelListOrClass = null, $ids = null)
+    {
+        (new GivesAbilities($this->store, [$this->authority], true))
+            ->group($this->scopeGroup)
+            ->parent($this->scopeParent)
+            ->to($abilities, $modelListOrClass, $ids);
+    }
+
+    /**
+     * Permit forbidden abilities from authorities.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|array|string $abilities
+     * @param \Illuminate\Database\Eloquent\Model|array|string|null $modelListOrClass
+     * @param array|string|int|null $ids
+     *
+     * @throws \InvalidArgumentException
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @throws \UnexpectedValueException
+     */
+    public function permit($abilities, $modelListOrClass = null, $ids = null)
+    {
+        (new RemovesAbilities($this->store, [$this->authority], true))
+            ->group($this->scopeGroup)
+            ->parent($this->scopeParent)
+            ->to($abilities, $modelListOrClass, $ids);
     }
 }
