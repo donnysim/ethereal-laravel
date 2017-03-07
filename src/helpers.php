8947cea5bb784a56cc121e77ce8a1957d495e1a6
donnysim <donatas.smk@gmail.com>
2017-03-07 09:02:47 +0200

    Add invalid helper.

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 if (!function_exists('valid')) {
     /**
-     * Determine if the given value is valid against given rules.
+     * Determine if the given value passes given rules.
      *
      * @param mixed $value
      * @param string|array $rules
@@ -11,5 +11,19 @@ if (!function_exists('valid')) {
      */
     function valid($value, $rules) {
         return validator(['value' => $value], ['value' => $rules])->fails() === false;
+    }
+}
+
+if (!function_exists('invalid')) {
+    /**
+     * Determine if the given value is fails to pass given rules.
+     *
+     * @param mixed $value
+     * @param string|array $rules
+     *
+     * @return bool
+     */
+    function invalid($value, $rules) {
+        return validator(['value' => $value], ['value' => $rules])->fails();
     }
 }
