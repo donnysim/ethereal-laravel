ff6ed17b5cd3ce6f71ee21a061c321125459b233
donnysim <donatas.smk@gmail.com>
2017-03-29 12:56:58 +0300

    Format and make data_intersect support asterisk.

--- src/helpers.php
+++ src/helpers.php
@@ -9,7 +9,8 @@ if (!function_exists('valid')) {
      *
      * @return bool
      */
-    function valid($value, $rules) {
+    function valid($value, $rules)
+    {
         return validator(['value' => $value], ['value' => $rules])->fails() === false;
     }
 }
@@ -23,7 +24,8 @@ if (!function_exists('invalid')) {
      *
      * @return bool
      */
-    function invalid($value, $rules) {
+    function invalid($value, $rules)
+    {
         return validator(['value' => $value], ['value' => $rules])->fails();
     }
 }
@@ -37,12 +39,48 @@ if (!function_exists('data_intersect')) {
      *
      * @return array
      */
-    function data_intersect($data, $keys) {
+    function data_intersect($data, $keys)
+    {
         $safe = [];
 
+        if (!$data) {
+            return $safe;
+        }
+
         foreach ($keys as $field) {
-            if (\Illuminate\Support\Arr::has($data, $field)) {
-                \Illuminate\Support\Arr::set($safe, $field, \Illuminate\Support\Arr::get($data, $field));
+            if (\Illuminate\Support\Str::contains($field, '*')) {
+                $parts = explode('.', $field);
+                $segment = implode('.', array_slice($parts, 0, array_search('*', $parts, true)));
+                $setSegment = implode('.', array_slice($parts, 0, array_search('*', $parts, true) + 1));
+
+                if ($segment === '') {
+                    // The field starts with an asterisk
+                    $result = [];
+
+                    foreach (array_values($data) as $index => $value) {
+                        $intersection = data_intersect($value, [implode('.', array_slice($parts, array_search('*', $parts, true) + 1))]);
+
+                        if (empty($intersection)) {
+                            continue;
+                        }
+
+                        \Illuminate\Support\Arr::set($result, str_replace('*', $index, $setSegment), $intersection);
+                    }
+
+                    $safe = array_replace_recursive($safe, $result);
+                } else {
+                    $result = data_intersect(\Illuminate\Support\Arr::get($data, $segment), [implode('.', array_slice($parts, array_search('*', $parts, true)))]);
+
+                    if (!empty($result)) {
+                        $tmp = [];
+                        \Illuminate\Support\Arr::set($tmp, $segment, $result);
+                        $safe = array_replace_recursive($safe, $tmp);
+                    }
+                }
+            } else {
+                if (\Illuminate\Support\Arr::has($data, $field)) {
+                    \Illuminate\Support\Arr::set($safe, $field, \Illuminate\Support\Arr::get($data, $field));
+                }
             }
         }
 
