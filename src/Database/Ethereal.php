a71ac245995ac93a3e325d2fcf4fd5673367c78c
Donatas <donatas.smk@gmail.com>
2017-05-30 20:26:09 +0300

    Merge cleanup.

--- src/Database/Ethereal.php
+++ src/Database/Ethereal.php
@@ -233,16 +233,4 @@ class Ethereal extends BaseModel
 
         return $this;
     }
-
-    /**
-     * Determine if the attribute is present in model.
-     *
-     * @param string $key
-     *
-     * @return bool
-     */
-    public function hasAttribute($key)
-    {
-        return array_key_exists($key, $this->attributes);
-    }
 }
